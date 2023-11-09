01bbb0259bac9802692c538566017d8d8312010d
unknown <aneildhakal21@gmail.com>
2023-11-09 12:28:20 -0500

    Model UI Integration

--- videocaptioningUI/frontend/views.py
+++ videocaptioningUI/frontend/views.py
@@ -4,3 +4,7 @@ from django.views import View
 class VideoCaptionView(View):
     def get(self, request):
         return render(request, "index.html", {})
+
+    def post(self, request):
+        print(request.FILES)
+        return self.get(request)
